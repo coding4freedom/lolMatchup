14f71b06a2d1765a9646ca2f284bf31630d808a3
coding4freedom <chawkins2010@hotmail.com>
2024-08-01 23:32:54 -0500

    Updated renderer to Best practice code, added preload file

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, ipcMain } from "electron";
+import path from 'path';
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const createWindow = () => {
-    let win = new BrowserWindow({
+    const win = new BrowserWindow({
         width: 500,
         height: 550,
         webPreferences: {
-            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            enableRemoteModule: false,
+            nodeIntegration: false,            
         }
     });
 
     win.loadFile('index.html');
 };
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+ipcMain.handle('fetch-data', async () => {
+    return ['Allice', 'Bob', 'James'];
+});
